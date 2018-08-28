b235fd08fb7092f7b15ebea978ebf0d55f9b634b
Wong Jia Hau <hou32hou@gmail.com>
2018-08-28 11:31:36 +0800

    Added scrollview for TaskForm

--- ui/TaskForm.js
+++ ui/TaskForm.js
@@ -9,7 +9,8 @@ import {
     TextInput,
     TimePickerAndroid,
     DatePickerAndroid,
-    ToastAndroid
+    ToastAndroid,
+    ScrollView
 } from "react-native";
 import { formatDate } from '../util';
 import { NULL_DATE } from '../js/Database';
@@ -25,72 +26,74 @@ export class TaskForm extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.header}>Title</Text>
-        <TextInput 
-          style={styles.textField}
-          value={this.state.task.title}
-          onChangeText={(title) => {this.setState({
-            task: {
-              ...this.state.task,
-              title: title
-            }
-          })}}
-          />
-
-        <Text style={styles.header}>Content</Text>
-        <TextInput 
-          style={styles.textField}
-          value={this.state.task.content}
-          multiline={true}
-          numberOfLines={3}
-          onChangeText={(content) => {this.setState({
-            task: {
-              ...this.state.task,
-              content: content
-            }
-          })}}
-          />
-
-        <Text style={styles.header}>Reminder</Text>
-        <View style={{flexDirection: "row", marginBottom: 40}}>
-          <TouchableOpacity
-            style={{paddingLeft: 10}}
-            onPress={this.handlePickDate}>
-            <View>
-              <Text style={styles.textField}>
-                {
-                  this.state.task.reminder !== NULL_DATE ?
-                  formatDate(new Date(this.state.task.reminder)) :
-                  "Click to add reminder"
-                }
-              </Text>
-            </View>
-          </TouchableOpacity>
-          {this.state.task.reminder === NULL_DATE ? null :
-          <TouchableOpacity style={{marginLeft: 10, marginTop: 3}} onPress={()=>{
-            this.setState({task: {...this.state.task, reminder: NULL_DATE}})
-            }}>
-              <Icon type='feather' name='x' />
-          </TouchableOpacity>}
-        </View>
-        <CustomButton 
-          text={this.props.buttonText}
-          onPress={() => {
-            if(!this.state.task.title) {
-              Alert.alert("Please fill in Title.")
-              return;
-            }
-            this.props.handleTask(this.state.task, (response) => {
-                ToastAndroid.show( 
-                  this.props.renderMessage(this.state.task),
-                  ToastAndroid.SHORT
-                );
-                this.props.navigation.goBack();
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.header}>Title</Text>
+          <TextInput 
+            style={styles.textField}
+            value={this.state.task.title}
+            onChangeText={(title) => {this.setState({
+              task: {
+                ...this.state.task,
+                title: title
               }
-            )
-          }} />
-      </View>
+            })}}
+            />
+
+          <Text style={styles.header}>Content</Text>
+          <TextInput 
+            style={styles.textField}
+            value={this.state.task.content}
+            multiline={true}
+            numberOfLines={3}
+            onChangeText={(content) => {this.setState({
+              task: {
+                ...this.state.task,
+                content: content
+              }
+            })}}
+            />
+
+          <Text style={styles.header}>Reminder</Text>
+          <View style={{flexDirection: "row", marginBottom: 40}}>
+            <TouchableOpacity
+              style={{paddingLeft: 10}}
+              onPress={this.handlePickDate}>
+              <View>
+                <Text style={styles.textField}>
+                  {
+                    this.state.task.reminder !== NULL_DATE ?
+                    formatDate(new Date(this.state.task.reminder)) :
+                    "Click to add reminder"
+                  }
+                </Text>
+              </View>
+            </TouchableOpacity>
+            {this.state.task.reminder === NULL_DATE ? null :
+            <TouchableOpacity style={{marginLeft: 10, marginTop: 3}} onPress={()=>{
+              this.setState({task: {...this.state.task, reminder: NULL_DATE}})
+              }}>
+                <Icon type='feather' name='x' />
+            </TouchableOpacity>}
+          </View>
+          <CustomButton 
+            text={this.props.buttonText}
+            onPress={() => {
+              if(!this.state.task.title) {
+                Alert.alert("Please fill in Title.")
+                return;
+              }
+              this.props.handleTask(this.state.task, (response) => {
+                  ToastAndroid.show( 
+                    this.props.renderMessage(this.state.task),
+                    ToastAndroid.SHORT
+                  );
+                  this.props.navigation.goBack();
+                }
+              )
+            }} />
+        </View>
+      </ScrollView>
     );
   }
 
